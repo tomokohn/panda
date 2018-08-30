747fef4670322cc7b9f394b4f71a84548d969851
tomer_kohn1 <tomer@finityx.com>
2018-08-31 01:05:51 +0300

    added redux

---
+++ comment-app/src/actions/types.js
@@ -0,0 +1,4 @@
+export const GET_COMMENTS = 'GET_COMMENTS';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const COMMENTS_LOADING = 'COMMENTS_LOADING';
